4e04eeec7edab6c6f1352e60a8d4e1dc91392b7e
ryantantiern <ryantantiern@gmail.com>
2016-11-28 03:11:36 +0000

    edit index

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,18 +4,17 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Profile</div>
 
-                <div class="panel-body">
-                    <img class="media-object" src="" alt="profile-pic"/>
-                    <p>{{ $user->firstname }}</p>
-                    <p>{{ $user->lastname }}</p>
-                    <p>{{ $user->email }}</p>
-                    <p>{{ $user->university->name }}</p>
+            <img src="/uploads/avatar/{{$user->avatar}}" alt="profile-pic" 
+             style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;"/>
+            <h2>{{ $user->firstname }}</h2>
+        
+            <p>{{ $user->firstname }}</p>
+            <p>{{ $user->lastname }}</p>
+            <p>{{ $user->email }}</p>
+            <p>{{ $user->university->name }}</p>
 
-                </div>
-            </div>
+             
         </div>
     </div>
 </div>
